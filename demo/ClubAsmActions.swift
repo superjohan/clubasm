f582b8e55e7b2f92de371dfa01745eda4d389fe2
Johan Halin <johan@halin.me>
2019-03-26 18:51:46 +0200

    Protocol for defining the flow in content views.

---
+++ demo/ClubAsmActions.swift
@@ -0,0 +1,15 @@
+//
+//  ClubAsmActions.swift
+//  demo
+//
+//  Created by Johan Halin on 26/03/2019.
+//  Copyright © 2019 Dekadence. All rights reserved.
+//
+
+protocol ClubAsmActions {
+    func action1()
+    func action2()
+    func action3()
+    func action4()
+    func action5()
+}
