d7a38199ff31d3fa1566a4ca12358ec5b33ef50f
Johan Halin <johan@halin.me>
2019-04-04 21:45:16 +0300

    Can't get the logo to work, but at least the stars are showing.

--- demo/part 2/ClubAsmAssemblyView.swift
+++ demo/part 2/ClubAsmAssemblyView.swift
@@ -10,17 +10,19 @@ import UIKit
 import SceneKit
 
 class ClubAsmAssemblyView: UIView, ClubAsmActions {
-    let sceneView = SCNView()
-    let camera = SCNNode()
+    private let sceneView = SCNView()
+    private let camera = SCNNode()
+    private let assemblyLogo = loadModel(name: "asm_a", textureName: nil, color: UIColor.init(white: 0.95, alpha: 1.0))
 
     override init(frame: CGRect) {
         let camera = SCNCamera()
         camera.zFar = 600
-//        camera.vignettingIntensity = 1
-//        camera.vignettingPower = 1
-//        camera.colorFringeStrength = 3
-//        camera.bloomIntensity = 1
-//        camera.bloomBlurRadius = 40
+        camera.vignettingIntensity = 1
+        camera.vignettingPower = 1
+        camera.colorFringeStrength = 2
+        camera.bloomIntensity = 1
+        camera.bloomBlurRadius = 40
+        camera.motionBlurIntensity = 1.5
         self.camera.camera = camera // lol
 
         super.init(frame: frame)
@@ -28,6 +30,7 @@ class ClubAsmAssemblyView: UIView, ClubAsmActions {
         self.sceneView.backgroundColor = .black
 //        self.sceneView.delegate = self
         self.sceneView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        addSubview(self.sceneView)
         
         self.sceneView.scene = createScene()
     }
@@ -36,7 +39,12 @@ class ClubAsmAssemblyView: UIView, ClubAsmActions {
         fatalError("init(coder:) has not been implemented")
     }
     
+    var animating = false
+    
     func action1() {
+        if self.animating { return }
+
+        self.animating = true
     }
     
     func action2() {
@@ -55,11 +63,18 @@ class ClubAsmAssemblyView: UIView, ClubAsmActions {
         let scene = SCNScene()
         scene.background.contents = UIColor.black
         
-        self.camera.position = SCNVector3Make(0, 0, 58)
-        
+        self.camera.position = SCNVector3Make(0, 10, 20)
+        self.camera.rotation = SCNVector4Make(-0.2, 0, 0, 0.5)
         scene.rootNode.addChildNode(self.camera)
         
+        self.assemblyLogo.position = SCNVector3Make(0, 0, 0)
+//        self.assemblyLogo.scale = SCNVector3Make(3, 3, 3)
+        scene.rootNode.addChildNode(self.assemblyLogo)
+
         configureLight(scene)
+        
+        let starField = SCNParticleSystem(named: "stars", inDirectory: nil)!
+        scene.rootNode.addParticleSystem(starField)
         
         return scene
     }
