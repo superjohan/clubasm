329ce6abe1a7ef70567dbc22dfe9343a271dd939
Johan Halin <johan@halin.me>
2019-04-06 15:58:08 +0300

    A lot more action, with some sort of movement during the quiet part.
    A green box is a placeholder for the assembly logo.

--- demo/part 2/ClubAsmAssemblyView.swift
+++ demo/part 2/ClubAsmAssemblyView.swift
@@ -12,8 +12,14 @@ import SceneKit
 class ClubAsmAssemblyView: UIView, ClubAsmActions {
     private let sceneView = SCNView()
     private let camera = SCNNode()
-    private let assemblyLogo = loadModel(name: "asm_a", textureName: nil, color: UIColor.init(white: 0.95, alpha: 1.0))
-
+    private let logoWrapper = SCNNode()
+//    private let assemblyLogo = loadModel(name: "asm_a", textureName: nil, color: UIColor.init(white: 0.95, alpha: 1.0))
+    private var assemblyLogo: SCNNode?
+    private var starField: SCNParticleSystem?
+    private var explosion: SCNParticleSystem?
+    
+    private var position = 0
+    
     override init(frame: CGRect) {
         let camera = SCNCamera()
         camera.zFar = 600
@@ -42,9 +48,43 @@ class ClubAsmAssemblyView: UIView, ClubAsmActions {
     var animating = false
     
     func action1() {
-        if self.animating { return }
+        if self.position == 0 {
+            let duration = ClubAsmConstants.barLength * 8
+            
+            let rotateAction = SCNAction.rotateTo(x: 0, y: 0, z: 0, duration: duration)
+            rotateAction.timingMode = .easeIn
+            self.camera.runAction(rotateAction)
+            
+            let cameraPositionAction = SCNAction.move(to: SCNVector3Make(0, 0, 20), duration: duration)
+            cameraPositionAction.timingMode = .easeOut
+            self.camera.runAction(cameraPositionAction)
+            
+            let logoRotateAction = SCNAction.rotateBy(x: 0, y: CGFloat.pi * 2, z: 0, duration: 5)
+            logoRotateAction.timingMode = .linear
+            self.assemblyLogo?.runAction(SCNAction.repeatForever(logoRotateAction))
+            
+            let logoPositionAction = SCNAction.move(to: SCNVector3Make(0, 0, 0), duration: duration)
+            logoPositionAction.timingMode = .easeOut
+            self.logoWrapper.runAction(logoPositionAction)
+        }
+        
+        if self.position == 4 {
+            self.starField?.loops = false
+        }
+        
+        if self.position == 6 {
+            let duration = ClubAsmConstants.barLength * 2
 
-        self.animating = true
+            let logoFlightRotationAction = SCNAction.rotateTo(x: 0, y: 0, z: 0, duration: duration)
+            logoFlightRotationAction.timingMode = .easeInEaseOut
+            self.logoWrapper.runAction(logoFlightRotationAction)
+        }
+        
+        if self.position == 8 {
+            self.sceneView.scene?.rootNode.addParticleSystem(self.explosion!)
+        }
+        
+        self.position += 1
     }
     
     func action2() {
@@ -64,17 +104,31 @@ class ClubAsmAssemblyView: UIView, ClubAsmActions {
         scene.background.contents = UIColor.black
         
         self.camera.position = SCNVector3Make(0, 10, 20)
-        self.camera.rotation = SCNVector4Make(-0.2, 0, 0, 0.5)
+        self.camera.rotation = SCNVector4Make(-0.2, 0, 0, 1)
         scene.rootNode.addChildNode(self.camera)
         
-        self.assemblyLogo.position = SCNVector3Make(0, 0, 0)
+//        self.assemblyLogo.position = SCNVector3Make(0, 0, 0)
 //        self.assemblyLogo.scale = SCNVector3Make(3, 3, 3)
-        scene.rootNode.addChildNode(self.assemblyLogo)
+//        scene.rootNode.addChildNode(self.assemblyLogo)
 
+        let box = SCNBox(width: 8, height: 11, length: 2, chamferRadius: 0)
+        box.firstMaterial?.diffuse.contents = UIColor.green
+        let boxNode = SCNNode(geometry: box)
+
+        self.assemblyLogo = boxNode
+
+        self.logoWrapper.position = SCNVector3Make(0, -20, 25)
+        self.logoWrapper.addChildNode(self.assemblyLogo!)
+        self.logoWrapper.rotation = SCNVector4Make(1, 0, 0, -Float.pi / 2.0)
+        scene.rootNode.addChildNode(self.logoWrapper)
+        
         configureLight(scene)
         
         let starField = SCNParticleSystem(named: "stars", inDirectory: nil)!
         scene.rootNode.addParticleSystem(starField)
+        self.starField = starField
+
+        self.explosion = SCNParticleSystem(named: "boom", inDirectory: nil)
         
         return scene
     }
