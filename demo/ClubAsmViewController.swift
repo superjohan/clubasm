d9fac12bae072c70971442c8a3242da345653b24
Johan Halin <johan@halin.me>
2019-04-11 18:31:46 +0300

    Third compo block view.

--- demo/ClubAsmViewController.swift
+++ demo/ClubAsmViewController.swift
@@ -135,6 +135,7 @@ class ClubAsmViewController: UIViewController {
         
         self.part3views.append(ClubAsmDemoIntroView(frame: frame))
         self.part3views.append(ClubAsmCompoCircleView(frame: frame))
+        self.part3views.append(ClubAsmCompoSquaresView(frame: frame))
         
         for view in self.part3views {
             self.part3view.addSubview(view)
@@ -268,15 +269,19 @@ class ClubAsmViewController: UIViewController {
         }
         
         if self.position == ClubAsmPositions.beatBassline2Start + 4 {
-            self.part3view.isHidden = false
             self.currentView = self.part3views[1]
         }
 
         if self.position == ClubAsmPositions.beatBassline2Start + 8 {
+            self.part3view.isHidden = false
+            self.currentView = self.part3views[2]
+        }
+
+        // FIXME: remove
+        if self.position == ClubAsmPositions.beatBassline2Start + 12 {
             self.part3view.isHidden = true
         }
-        
-        // FIXME: remove
+
         if currentView !== self.currentView {
             currentView?.isHidden = true
             self.currentView?.isHidden = false
--- demo/part 3/ClubAsmCompoSquaresView.swift
+++ demo/part 3/ClubAsmCompoSquaresView.swift
@@ -8,14 +8,173 @@
 
 import UIKit
 
-class ClubAsmCompoSquaresView: UIView {
+class ClubAsmCompoSquaresView: UIView, ClubAsmActions {
+    private let border: CGFloat = 100
+    private let baseline: CGFloat = 15
+    private let box = UIView()
+    private let imageView1 = UIImageView()
+    private let imageView2 = UIImageView()
+    private let imageView3 = UIImageView()
+    private let imageView4 = UIImageView()
+    private let images: [UIImage]
+    
+    private var position = 0
+    
+    override init(frame: CGRect) {
+        var images = [UIImage]()
+        images.append(UIImage(named: "clubasmcompo_3-4")!)
+        images.append(UIImage(named: "clubasmcompo_3-3")!)
+        images.append(UIImage(named: "clubasmcompo_3-2")!)
+        images.append(UIImage(named: "clubasmcompo_3-1")!)
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+        self.images = images
+
+        super.init(frame: frame)
+        
+        self.backgroundColor = .white
+        
+        self.box.frame = CGRect(
+            x: self.bounds.minX + self.border,
+            y: self.bounds.minY + self.border,
+            width: self.bounds.size.width - (self.border * 2.0),
+            height: self.bounds.size.height - (self.border * 2.0)
+        )
+        self.box.backgroundColor = .lightGray
+        
+        let width: CGFloat = images[0].size.width
+        let height: CGFloat = images[0].size.height
+        
+        self.imageView1.frame = CGRect(
+            x: 0,
+            y: self.box.bounds.minY - height + self.baseline,
+            width: width,
+            height: height
+        )
+        self.box.addSubview(self.imageView1)
+
+        self.imageView2.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: width,
+            height: height
+        )
+        self.imageView2.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi * 0.5)
+        setImageView2origin()
+        self.box.addSubview(self.imageView2)
+
+        self.imageView3.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: width,
+            height: height
+        )
+        self.imageView3.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi)
+        setImageView3origin()
+        self.box.addSubview(self.imageView3)
+
+        self.imageView4.frame = CGRect(
+            x: 0,
+            y: 0,
+            width: width,
+            height: height
+        )
+        self.imageView4.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi * 1.5)
+        setImageView4origin()
+        self.box.addSubview(self.imageView4)
+
+        updatePosition()
+        
+        addSubview(self.box)
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func action1() {
+    }
+    
+    func action2() {
+    }
+    
+    func action3() {
+    }
+    
+    func action4() {
+    }
+    
+    func action5() {
+        UIView.animate(withDuration: 1.5, delay: 0, options: [.curveEaseOut], animations: {
+            self.box.frame.size.width = self.bounds.size.height - (self.border * 2.0)
+            self.box.frame.size.height = self.bounds.size.width - (self.border * 2.0)
+            self.box.frame.origin.x = self.bounds.midX - (self.box.bounds.size.width / 2.0)
+            self.box.frame.origin.y = self.bounds.midY - (self.box.bounds.size.height / 2.0)
+            self.box.transform = self.box.transform.rotated(by: CGFloat.pi * 0.5)
 
+            self.setImageView2origin()
+            self.setImageView3origin()
+            self.setImageView4origin()
+        }, completion: { done in
+            self.box.transform = .identity
+            self.box.frame = CGRect(
+                x: self.bounds.minX + self.border,
+                y: self.bounds.minY + self.border,
+                width: self.bounds.size.width - (self.border * 2.0),
+                height: self.bounds.size.height - (self.border * 2.0)
+            )
+            
+            self.setImageView2origin()
+            self.setImageView3origin()
+            self.setImageView4origin()
+
+            self.updatePosition()
+        })
+    }
+    
+    private func setImageView2origin() {
+        self.imageView2.frame.origin.x = self.box.bounds.size.width - self.baseline
+        self.imageView2.frame.origin.y = self.box.bounds.minY
+    }
+    
+    private func setImageView3origin() {
+        self.imageView3.frame.origin.x = self.box.bounds.size.width - self.imageView3.bounds.size.width
+        self.imageView3.frame.origin.y = self.box.bounds.size.height - self.baseline
+    }
+    
+    private func setImageView4origin() {
+        self.imageView4.frame.origin.x = self.box.bounds.minX - self.imageView4.bounds.size.height + self.baseline
+        self.imageView4.frame.origin.y = self.box.bounds.maxY - self.imageView4.bounds.size.width
+    }
+    
+    private func updatePosition() {
+        self.position += 1
+        if self.position >= 4 {
+            self.position = 0
+        }
+        
+        switch self.position {
+        case 0:
+            self.imageView1.image = self.images[0]
+            self.imageView2.image = self.images[1]
+            self.imageView3.image = self.images[2]
+            self.imageView4.image = self.images[3]
+        case 1:
+            self.imageView1.image = self.images[3]
+            self.imageView2.image = self.images[0]
+            self.imageView3.image = self.images[1]
+            self.imageView4.image = self.images[2]
+        case 2:
+            self.imageView1.image = self.images[2]
+            self.imageView2.image = self.images[3]
+            self.imageView3.image = self.images[0]
+            self.imageView4.image = self.images[1]
+        case 3:
+            self.imageView1.image = self.images[1]
+            self.imageView2.image = self.images[2]
+            self.imageView3.image = self.images[3]
+            self.imageView4.image = self.images[0]
+        default:
+            abort()
+        }
+    }
 }
