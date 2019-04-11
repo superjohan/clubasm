142b64bc100c9a2e359b6a74d82786cff5fbe939
Johan Halin <johan@halin.me>
2019-04-11 17:26:56 +0300

    Image assets for the third compo block.

---
+++ demo/part 3/ClubAsmCompoSquaresView.swift
@@ -0,0 +1,21 @@
+//
+//  ClubAsmCompoSquaresView.swift
+//  demo
+//
+//  Created by Johan Halin on 11/04/2019.
+//  Copyright © 2019 Dekadence. All rights reserved.
+//
+
+import UIKit
+
+class ClubAsmCompoSquaresView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
