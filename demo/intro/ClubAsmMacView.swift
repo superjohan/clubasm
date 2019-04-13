c39575e066607ae6035d751d64c938ad36a03bd5
Johan Halin <johan@halin.me>
2019-04-13 16:29:17 +0300

    "Classic Mac" view assets.

---
+++ demo/intro/ClubAsmMacView.swift
@@ -0,0 +1,21 @@
+//
+//  ClubAsmMacView.swift
+//  demo
+//
+//  Created by Johan Halin on 13/04/2019.
+//  Copyright © 2019 Dekadence. All rights reserved.
+//
+
+import UIKit
+
+class ClubAsmMacView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
