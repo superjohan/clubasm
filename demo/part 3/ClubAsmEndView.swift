4e049e6dc4905e14a56238cf54378c9609ee8ee8
Johan Halin <johan@halin.me>
2019-04-14 16:25:24 +0300

    End logo assets.

---
+++ demo/part 3/ClubAsmEndView.swift
@@ -0,0 +1,21 @@
+//
+//  ClubAsmEndView.swift
+//  demo
+//
+//  Created by Johan Halin on 14/04/2019.
+//  Copyright © 2019 Dekadence. All rights reserved.
+//
+
+import UIKit
+
+class ClubAsmEndView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
