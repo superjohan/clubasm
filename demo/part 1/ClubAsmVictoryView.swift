86ca9f83265de5d63216164d0eaf03aae3bb325f
Johan Halin <johan@halin.me>
2019-04-15 16:07:45 +0300

    A background for the "victory" view.

--- demo/part 1/ClubAsmVictoryView.swift
+++ demo/part 1/ClubAsmVictoryView.swift
@@ -11,13 +11,33 @@ import UIKit
 class ClubAsmVictoryView: UIView, ClubAsmActions {
     private var textImages = [UIImageView]()
     private var sines = [UIView]()
+    private let background = UIView()
     
     private var animating = false
+    private var sineWidth: Double = 0
     
     override init(frame: CGRect) {
         super.init(frame: frame)
 
         self.backgroundColor = .white
+
+        self.background.frame = self.bounds
+        self.background.backgroundColor = UIColor(red:1.000, green:0.919, blue:0.894, alpha:1.000)
+        
+        let filledSine = UIImage(named: "clubasmfilledsine")!
+        self.background.frame.size.width += filledSine.size.width
+        let borderView = UIView(frame: CGRect(x: 0, y: (self.bounds.size.height / 2.0) - (filledSine.size.height / 2.0), width: self.bounds.size.width + filledSine.size.width, height: filledSine.size.height))
+        borderView.backgroundColor = UIColor(patternImage: filledSine)
+        self.sineWidth = Double(filledSine.size.width)
+        self.background.addSubview(borderView)
+        
+        let lowerHalf = UIView(frame: CGRect(x: 0, y: borderView.frame.maxY, width: self.bounds.size.width + filledSine.size.width, height: self.bounds.size.height - borderView.frame.maxY))
+        lowerHalf.backgroundColor = .white
+        self.background.addSubview(lowerHalf)
+        
+        self.background.alpha = 0
+        
+        addSubview(self.background)
 
         guard let image = UIImage(named: "clubasmvictory") else { return }
         guard let sineImage = UIImage(named: "clubasmsine")?.withRenderingMode(.alwaysTemplate) else { return }
@@ -129,6 +149,21 @@ class ClubAsmVictoryView: UIView, ClubAsmActions {
     func action5() {
         for i in 0..<self.textImages.count {
             setVisibility(index: i, visible: true, showSine: true)
+        }
+        
+        if !self.animating {
+            let centerX: Double = Double(self.bounds.size.width / 2.0) + (self.sineWidth / 2.0)
+            let animation = CABasicAnimation(keyPath: "position.x")
+            animation.fromValue = NSNumber(floatLiteral: centerX)
+            animation.toValue = NSNumber(floatLiteral: centerX - self.sineWidth)
+            animation.duration = 2
+            animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+            animation.repeatCount = Float.infinity
+            self.background.layer.add(animation, forKey: "xposition")
+            
+            UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+                self.background.alpha = 1
+            }, completion: nil)
         }
         
         self.animating = true
