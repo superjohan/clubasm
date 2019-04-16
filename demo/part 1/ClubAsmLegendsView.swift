acbe3f19a2af6ea8208d0c12cb5ef9b0e7475df6
Johan Halin <johan@halin.me>
2019-04-16 16:07:39 +0300

    Improved colors and added animated background.

--- demo/part 1/ClubAsmLegendsView.swift
+++ demo/part 1/ClubAsmLegendsView.swift
@@ -12,6 +12,7 @@ class ClubAsmLegendsView: UIView, ClubAsmActions {
     private let imageCount = 8
     
     private let mainImage: UIImageView
+    private let bg = UIView()
     
     private var images = [UIImageView]()
     private var distance = CGFloat(0)
@@ -36,9 +37,9 @@ class ClubAsmLegendsView: UIView, ClubAsmActions {
         self.mainImage.tintColor = .white
         addSubview(self.mainImage)
         
-        let red: CGFloat = 0.621
-        let green: CGFloat = 0
-        let blue: CGFloat = 0.003
+        let red: CGFloat = 0.342
+        let green: CGFloat = 0.078
+        let blue: CGFloat = 0.000
 
         for i in 0..<self.imageCount {
             let imageView = UIImageView(image: image)
@@ -59,6 +60,28 @@ class ClubAsmLegendsView: UIView, ClubAsmActions {
             
             self.images.append(imageView)
         }
+        
+        self.bg.frame = CGRect(x: 0, y: (self.bounds.size.height / 2.0) - (self.bounds.size.width / 2.0), width: self.bounds.size.width, height: self.bounds.size.width)
+        
+        let segmentHeight = self.bg.bounds.size.height / CGFloat(self.imageCount)
+        
+        for i in 0..<self.imageCount {
+            let color = self.images[self.imageCount - i - 1].tintColor
+            let view = UIView(frame:
+                CGRect(
+                    x: 0,
+                    y: self.bg.bounds.size.height,
+                    width: self.bg.bounds.size.width,
+                    height: segmentHeight
+                )
+            )
+            view.backgroundColor = color
+            self.bg.addSubview(view)
+        }
+
+        self.bg.transform = CGAffineTransform.identity.rotated(by: -CGFloat.pi * 0.25).scaledBy(x: 1, y: 0.49)
+        self.bg.clipsToBounds = true
+        insertSubview(self.bg, at: 0)
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -69,6 +92,10 @@ class ClubAsmLegendsView: UIView, ClubAsmActions {
         self.mainImage.isHidden = false
         self.mainImage.frame.origin.x = 0
 
+        for view in self.bg.subviews {
+            view.frame.origin.y = self.bg.bounds.size.height
+        }
+        
         for view in self.images {
             view.isHidden = true
             view.frame = self.mainImage.frame
@@ -92,11 +119,15 @@ class ClubAsmLegendsView: UIView, ClubAsmActions {
         
         for (index, view) in self.images.enumerated() {
             view.isHidden = false
+        
+            let segment = self.bg.subviews[index]
             
             let ratio = CGFloat(index) / CGFloat(self.imageCount - 1)
+            let segmentRatio = CGFloat(index) / CGFloat(self.imageCount)
             
             UIView.animate(withDuration: ClubAsmConstants.animationDuration, delay: 0, options: [.curveEaseOut], animations: {
                 view.frame.origin.x = self.distance * ratio
+                segment.frame.origin.y = self.bg.bounds.size.height * segmentRatio
             }, completion: nil)
         }
     }
